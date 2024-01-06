7da5098adfeb7fbf3b73edd1cd636363238bd5b6
Maxim <88023333+664member@users.noreply.github.com>
2024-01-06 17:31:34 +0300

    Update NC_ferm.py

--- NC_ferm.py
+++ NC_ferm.py
@@ -22,6 +22,9 @@ if __name__ == '__main__':
     print('Press Ctrl-C to quit.')
     startX = 960
     startY = 720
+    clickerSpeed = 0.0005
+    startX = 960
+    startY = 720
     clickerSpeed = 0.65
     try:
         X = pg.prompt('Enter','startX', startX)
@@ -35,13 +38,14 @@ if __name__ == '__main__':
             clickerSpeed = float(sp)        
         while(True):
             if(pg.position().x > startX + 50 or pg.position().x < startX-50 or pg.position().y > startY+10 or pg.position().y < startY-50):
-                pg.moveTo(startX,startY, 0.2, pg.easeOutQuad)
-            pg.move(random.randint(1,20)-random.randint(1,20) , random.randint(1,10)-random.randint(1,10),0.25)
+                pg.moveTo(startX,startY, clickerSpeed, pg.easeOutQuad)
+            pg.move(random.randint(1,20)-random.randint(1,20) , random.randint(1,10)-random.randint(1,10),clickerSpeed)
             pg.click(clicks=2, interval = clickerSpeed)
             if(msvcrt.kbhit()):
                 break
     except KeyboardInterrupt:
         print('\n') 
+
 
 
 
