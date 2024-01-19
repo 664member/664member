751f8e05248825b9b121584277916a03438b300a
Maxim <88023333+664member@users.noreply.github.com>
2024-01-19 23:40:09 +0300

    Update minOperations.js

--- minOperations.js
+++ minOperations.js
@@ -4,7 +4,6 @@ var minOperations = function(nums) {
     for(let i of nums){
         counter[i] ? counter[i] += 1 : counter[i] =1 
     }
-
     for(let i of Object.keys(counter)){
         if(counter[i] === 1) return -1
         //we can deduce that the number of operations needed to remove a total of count elements of a given kind is represented by the expression ceil(count /  3)
